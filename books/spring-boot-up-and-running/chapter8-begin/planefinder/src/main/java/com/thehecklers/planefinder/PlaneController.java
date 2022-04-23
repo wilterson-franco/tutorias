d182ca7d0a9684b82603b86a945c117310e32ccd
Wilterson Franco <wilterson.franco@hotmail.com>
2022-04-23 00:44:29 -0400

    books/spring-boot-up-and-running
    Chapter 8: planefinder project conversion to non-blocking by using Spring Webflux

--- books/spring-boot-up-and-running/chapter8-begin/planefinder/src/main/java/com/thehecklers/planefinder/PlaneController.java
+++ books/spring-boot-up-and-running/chapter8-begin/planefinder/src/main/java/com/thehecklers/planefinder/PlaneController.java
@@ -1,13 +1,14 @@
 package com.thehecklers.planefinder;
 
+import java.io.IOException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.io.IOException;
+import reactor.core.publisher.Flux;
 
 @Controller
 public class PlaneController {
+
     private final PlaneFinderService pfService;
 
     public PlaneController(PlaneFinderService pfService) {
@@ -16,7 +17,7 @@ public class PlaneController {
 
     @ResponseBody
     @GetMapping("/aircraft")
-    public Iterable<Aircraft> getCurrentAircraft() throws IOException {
+    public Flux<Aircraft> getCurrentAircraft() throws IOException {
         return pfService.getAircraft();
     }
 }
--- books/spring-boot-up-and-running/chapter8-begin/planefinder/src/main/java/com/thehecklers/planefinder/PlaneRepository.java
+++ books/spring-boot-up-and-running/chapter8-begin/planefinder/src/main/java/com/thehecklers/planefinder/PlaneRepository.java
@@ -1,6 +1,7 @@
 package com.thehecklers.planefinder;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
-public interface PlaneRepository extends CrudRepository<Aircraft, Long> {
+public interface PlaneRepository extends ReactiveCrudRepository<Aircraft, Long> {
 }
