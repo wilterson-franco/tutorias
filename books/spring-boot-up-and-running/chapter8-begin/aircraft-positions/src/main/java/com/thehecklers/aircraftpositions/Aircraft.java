6c25b078c0e3d4a8b4c9ea1af45d910e402f255d
Wilterson Franco <wilterson.franco@hotmail.com>
2022-04-24 01:21:04 -0400

    books/spring-boot-up-and-running
    Chapter 8: aircraftpositions project conversion to non-blocking by using Spring Webflux

--- books/spring-boot-up-and-running/chapter8-begin/aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/Aircraft.java
+++ books/spring-boot-up-and-running/chapter8-begin/aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/Aircraft.java
@@ -1,19 +1,19 @@
 package com.thehecklers.aircraftpositions;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.Instant;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import java.time.Instant;
-
-@Entity
+@Document
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Aircraft {
+
     @Id
     private Long id;
     private String callsign, squawk, reg, flightno, route, type, category;
--- books/spring-boot-up-and-running/chapter8-begin/aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/PositionController.java
+++ books/spring-boot-up-and-running/chapter8-begin/aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/PositionController.java
@@ -6,27 +6,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 @RequiredArgsConstructor
 @Controller
 public class PositionController {
+
     @NonNull
     private final AircraftRepository repository;
-    private WebClient client =
-            WebClient.create("http://localhost:7634/aircraft");
+    private WebClient client = WebClient.create("http://localhost:7634/aircraft");
 
     @GetMapping("/aircraft")
     public String getCurrentAircraftPositions(Model model) {
-        repository.deleteAll();
 
-        client.get()
-                .retrieve()
-                .bodyToFlux(Aircraft.class)
-                .filter(plane -> !plane.getReg().isEmpty())
-                .toStream()
-                .forEach(repository::save);
+        Flux<Aircraft> aircraftFlux = repository.deleteAll()
+                .thenMany(client.get()
+                        .retrieve()
+                        .bodyToFlux(Aircraft.class)
+                        .filter(plane -> !plane.getReg().isEmpty())
+                        .flatMap(repository::save));
 
-        model.addAttribute("currentPositions", repository.findAll());
+        model.addAttribute("currentPositions", aircraftFlux);
+
         return "positions";
     }
 }
